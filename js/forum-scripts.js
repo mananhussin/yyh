1784f4f20ae331010e1498df175ac04d6d336e3f
Zach | xDest <destroyer7712@gmail.com>
2017-08-17 21:55:41 -0400

    Forum Group Effect
    - Added slide in / out effect to forum groups
    - Added hover effect to forum groups
    - Added side picture to fill left 2 columns, can be changed

---
+++ js/forum-scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+    //Toggle different forum sections
+    $(".group-head").click(function(e) {
+        $(e.target.nextElementSibling).slideToggle();
+        //$(".group-head").next().slideToggle();
+    });
+    
+    
+    //On hover expand
+    $(".group-head").hover(function(e) {
+        $(e.target).addClass("group-head-hover");
+    });
+    
+    //On exit reduce
+    $(".group-head").mouseout(function(e) {
+        $(e.target).removeClass("group-head-hover");
+    });
+});
