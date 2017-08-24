16ea40dea2890929d41b79fdd88baefb5fa4347a
Zach | xDest <destroyer7712@gmail.com>
2017-08-24 19:35:19 -0400

    Text Parser
    The function converRawTextToSafeText(str) returns a string which can be saved into the database. The string saves line breaks and greater than / less than signs.

--- js/util.js
+++ js/util.js
@@ -191,3 +191,63 @@ function createRepliesFromPostReplyData(data) {
     }
     return dataAsReplies;
 }
+
+
+
+//Find illegal characters in string. This is mainly to be more safe when allowing uses to create posts. (Some users could add <input id="...".>> to fool people)
+//This method returns the indicies of each illegal character.
+function findIllegalChars(str) {
+    var illegalChars = ['<','>'];
+    var illegalIndicies = [];
+    //As far as I can tell, these are the only illegal characters we need.
+    for(var i = 0; i < str.length; i++) {
+        for(var x = 0; x < illegalChars.length; x++) {
+            if(str.charAt(i) === illegalChars[x]) {
+                illegalIndicies.push(i);
+            }
+        }
+    }
+    return illegalIndicies;
+}
+
+
+
+function getIllegalReplacement(char) {
+    var illegalChars = ['<','>'];
+    var safeMatchups = ["&lt;","&gt;"];
+    for(var i = 0; i < illegalChars.length; i++) {
+        //This is so bad but i cant remember how to make keys / values
+        if(char === illegalChars[i]) {
+            return safeMatchups[i];
+        }
+    }
+    return char;
+}
+
+function stringReplaceAt(str, index, replacement) {
+    return str.substring(0,index) + replacement + str.substring(index+1, str.length);
+}
+
+function addLineBreaks(str) {
+	var newLines = str.split("\n");
+  var newStr = newLines[0]+"<br/>";
+  for (var i = 1; i < newLines.length-1; i++) {
+  	newStr+=newLines[i];
+    newStr+="<br/>";
+  }
+  newStr+=newLines[newLines.length-1];
+  return newStr;
+}
+
+//This replaces illegal characters with the html short cuts and line breaks with <br>
+function convertRawTextToSafeText(str) {
+    var toChange = findIllegalChars(str);
+    for(var i = toChange.length-1; i >= 0; i--) {
+    		$("#t").append(i + "  " + toChange[i]);
+        var badChar = str.charAt(toChange[i]);
+        var newChar = getIllegalReplacement(badChar);
+        str = stringReplaceAt(str, toChange[i], newChar);
+    }
+    str = addLineBreaks(str);
+    return str;
+}
